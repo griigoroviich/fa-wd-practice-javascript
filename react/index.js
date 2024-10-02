3f91f8f126175b6106fcd04044d67ddf21f03c6c
griigoroviich <griigoroviich@gmail.com>
2024-10-02 22:05:09 +0300

    test

--- react/index.js
+++ react/index.js
@@ -4,7 +4,7 @@ function NavigationBar() {
   // TODO: Actually implement a navigation bar
   return <h1>Hello from React!</h1>;
 }
-
+// dadada
 const domNode = document.getElementById('navigation');
 const root = createRoot(domNode);
 root.render(<NavigationBar />);
